79065cb5d3851793db2439e48eaa65393d995d34
Sophia <phuongmai001@gmail.com>
2018-07-02 19:31:03 -0700

    update modal.js

--- modal.js
+++ modal.js
@@ -1,11 +1,11 @@
 // Will hold previously focused element
-var focusedElementBeforeModal;
+let focusedElementBeforeModal;
 
 // Find the modal and its overlay
-var modal = document.querySelector('.modal');
-var modalOverlay = document.querySelector('.modal-overlay');
+const modal = document.querySelector('.modal');
+const modalOverlay = document.querySelector('.modal-overlay');
 
-var modalToggle = document.querySelector('.modal-toggle');
+const modalToggle = document.querySelector('.modal-toggle');
 modalToggle.addEventListener('click', openModal);
 
 function openModal() {
@@ -18,17 +18,17 @@ function openModal() {
   // Listen for indicators to close the modal
   modalOverlay.addEventListener('click', closeModal);
   // Sign-Up button
-  var signUpBtn = modal.querySelector('#signup');
-  signUpBtn.addEventListener('click', closeModal);
+  const sendBtn = modal.querySelector('#send');
+  sendBtn.addEventListener('click', closeModal);
 
   // Find all focusable children
-  var focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
-  var focusableElements = modal.querySelectorAll(focusableElementsString);
+  const focusableElementsString = 'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]';
+  let focusableElements = modal.querySelectorAll(focusableElementsString);
   // Convert NodeList to Array
   focusableElements = Array.prototype.slice.call(focusableElements);
 
-  var firstTabStop = focusableElements[0];
-  var lastTabStop = focusableElements[focusableElements.length - 1];
+  const firstTabStop = focusableElements[0];
+  const lastTabStop = focusableElements[focusableElements.length - 1];
 
   // Show the modal and overlay
   modal.style.display = 'block';
